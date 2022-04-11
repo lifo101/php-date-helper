f36be5ceb98f080bab85871a8623ec3773885c6f
Jason Morriss <lifo101@gmail.com>
2022-04-11 13:19:40 -0400

    added a couple helper methods.

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -2,6 +2,7 @@
 
 namespace Lifo\PhpDateHelper;
 
+use DateInterval;
 use DateTime;
 use DateTimeZone;
 
@@ -110,5 +111,89 @@ class DateHelper
     {
         if (!$when) return false;
         return $when instanceof DateTime || preg_match('/^\d\d\d\d-\d\d-\d\d(T?\d\d:\d\d:\d\d)?/', $when);
+    }
+
+    /**
+     * Returns a human readable expression of the date interval given up to the total $parts specified.
+     * Seconds are NOT included if years,mon or days are present.
+     *
+     * @param DateInterval|DateTime|string $interval The date or interval to convert
+     * @param int                          $parts    Number of interval parts to include.
+     *
+     * @return string
+     */
+    public static function timeAgo($interval, int $parts = 3): string
+    {
+        if (!$interval instanceof DateInterval) {
+            if (!$interval instanceof DateTime) {
+                $interval = self::create($interval);
+            }
+            $interval = $interval->diff(new DateTime('now'));
+        }
+
+        $format = [];
+        if ($interval->y) $format[] = '%y year' . ($interval->y != 1 ? 's' : '');
+        if ($interval->m) $format[] = '%m month' . ($interval->m != 1 ? 's' : '');
+        if ($interval->d) {
+            if ($interval->d > 6) {
+                $weeks = floor($interval->d / 7);
+                $days = ceil($interval->d % 7);
+                $format[] = $weeks . ' week' . ($weeks != 1 ? 's' : '');
+                if ($days) {
+                    $format[] = $days . ' day' . ($days != 1 ? 's' : '');
+                }
+            } else {
+                $format[] = '%d day' . ($interval->d != 1 ? 's' : '');
+            }
+        }
+        if ($interval->h) $format[] = '%h hour' . ($interval->h != 1 ? 's' : '');
+        if ($interval->i) $format[] = '%i minute' . ($interval->i != 1 ? 's' : '');
+        if ($interval->s and !($interval->y or $interval->m or $interval->d)) $format[] = '%s second' . ($interval->s != 1 ? 's' : '');
+        $fmt = implode(', ', array_slice($format, 0, $parts));
+        return $interval->format($fmt);
+    }
+
+    /**
+     * Returns a short time value depending on how long ago it was to the current time
+     *
+     * @param DateTime|string $time   The time to convert
+     * @param string|null     $now    If null, the current time is used
+     * @param array|null      $format An array of formats to use for each time period.
+     *                                The keys are the time periods, and the values are the formats.
+     *                                The default is:
+     *                                [
+     *                                  'today' => 'g:i a',
+     *                                  'year'  => 'M j',
+     *                                  'other' => 'm/d/Y',
+     *                                ]
+     *
+     * @return string
+     */
+    public static function timeWhen($time, ?string $now = null, ?array $format = null): string
+    {
+        if (!($time instanceof DateTime)) {
+            $time = date_create((string)$time) ?: date_create();
+        }
+        $now = date_create($now ?? 'now');
+
+        $format = array_replace([
+            'today' => 'g:i a',
+            'year'  => 'M j',
+            'other' => 'm/d/Y',
+        ], $format ?? []);
+        switch (true) {
+            // TODAY: "3:00pm"
+            case ($time->format('Y-m-d') == $now->format('Y-m-d')):
+                $fmt = $format['today'];
+                break;
+            // THIS YEAR: "Jan 1"
+            case ($time->format('Y') == $now->format('Y')):
+                $fmt = $format['year'];
+                break;
+            // ANOTHER YEAR: "1/1/2022"
+            default:
+                $fmt = $format['other'];
+        }
+        return $time->format($fmt);
     }
 }
